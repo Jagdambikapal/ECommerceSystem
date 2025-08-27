8011c5397bef4ec605317e8e91cc1e63ef9c8cf2
jagdambikaPal <jagdambikap100@gmail.com>
2025-08-27 12:29:18 +0530

    Add project files.

---
+++ UserService.Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Xml.Linq;
+namespace UserService.Domain.Entities
+{
+    
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string? UserName { get; set; } = null!;
+        public string? Email { get; set; } = null!;
+        public bool IsEmailConfirmed { get; set; }
+        public bool IsActive { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? FullName { get; set; }
+        public string? ProfilePhotoUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? LastLoginAt { get; set; }
+        public List<Address> Addresses { get; set; } = new();
+    }
+}
